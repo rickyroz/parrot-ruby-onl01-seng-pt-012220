4824b24d43570df009a0c2913c6236ea1b6145a4
Ricky Rozier <rickydrozier@gmail.com>
2020-01-30 23:39:58 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def Parrot (arg ='Squawk!')
+def parrot (arg ='Squawk!')
   puts arg
   return arg
 end
 
-Parrot("bird")
+parrot("bird")
